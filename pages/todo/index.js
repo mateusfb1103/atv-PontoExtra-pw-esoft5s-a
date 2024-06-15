e43fa33a55c2f871470eef5e91ad18307362f6a6
mateusfb1103 <141289452+mateusfb1103@users.noreply.github.com>
2024-06-15 01:04:01 +0000

    create

---
+++ pages/todo/index.js
@@ -0,0 +1,120 @@
+const taskKey = "@tasks";
+
+let selectedTaskId = null;
+
+// Função para adicionar tarefa
+function addTask(event) {
+  event.preventDefault(); // Evita o recarregamento da página
+  const taskId = new Date().getTime();
+  const taskList = document.querySelector("#taskList");
+
+  const form = document.querySelector("#taskForm");
+  const formData = new FormData(form);
+
+  const taskTitle = formData.get("title");
+  const taskDescription = formData.get("description");
+
+  const li = document.createElement("li");
+
+  li.id = `id-${taskId}`;
+  li.innerHTML = `
+    <div>
+      <h2>${taskTitle}</h2>
+      <p>${taskDescription}</p>
+    </div>
+    <button title="Editar tarefa" onClick="openEditDialog(${taskId})">✏️</button>
+    <button class="remove-task" title="Excluir tarefa" onClick="deleteTask(${taskId})">❌</button>
+  `;
+
+  taskList.appendChild(li);
+
+  // Salvar tarefas no localStorage
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+  tasks.push({
+    id: taskId,
+    title: taskTitle,
+    description: taskDescription,
+  });
+  localStorage.setItem(taskKey, JSON.stringify(tasks));
+
+  form.reset();
+}
+
+function openEditDialog(taskId) {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+
+  selectedTaskId = tasks.findIndex((task) => task.id === taskId);
+  const task = tasks[selectedTaskId];
+
+  const dialog = document.querySelector("dialog");
+
+  const editTitle = document.querySelector("#editTaskForm #title");
+  const editDescription = document.querySelector("#editTaskForm #description");
+
+  editTitle.value = task.title;
+  editDescription.value = task.description;
+
+  dialog.showModal();
+}
+
+function editTask(event) {
+  event.preventDefault();
+
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+
+  const form = document.querySelector("#editTaskForm");
+  const formData = new FormData(form);
+
+  tasks[selectedTaskId] = {
+    ...tasks[selectedTaskId],
+    title: formData.get("title"),
+    description: formData.get("description"),
+  };
+
+  localStorage.setItem(taskKey, JSON.stringify(tasks));
+
+  const li = document.querySelector(`#id-${tasks[selectedTaskId].id}`);
+  li.querySelector("h2").textContent = tasks[selectedTaskId].title;
+  li.querySelector("p").textContent = tasks[selectedTaskId].description;
+
+  closeDialog();
+}
+
+function deleteTask(taskId) {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+  const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+  tasks.splice(taskIndex, 1);
+  localStorage.setItem(taskKey, JSON.stringify(tasks));
+
+  const li = document.querySelector(`#id-${taskId}`);
+
+  li.remove();
+  closeDialog();
+}
+
+function closeDialog() {
+  const dialog = document.querySelector("dialog");
+  dialog.close();
+}
+
+// Carregar tarefas do localStorage ao recarregar a página
+window.addEventListener("DOMContentLoaded", () => {
+  const tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+  const taskList = document.querySelector("#taskList");
+
+  taskList.innerHTML = tasks
+    .map(
+      (task) => `
+      <li id='id-${task.id}'>
+        <div>
+          <h2>${task.title}</h2>
+          <p>${task.description}</p>
+        </div>
+        <button title="Editar tarefa" onClick="openEditDialog(${task.id})">✏️</button>
+        <button class="remove-task" title="Excluir tarefa" onClick="deleteTask(${task.id})">❌</button>
+      </li>
+    `
+    )
+    .join("");
+});
